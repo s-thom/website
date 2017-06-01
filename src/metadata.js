f05d0e736f102319bd278a81a4fe1730aa442054
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-02 11:09:55 +1200

    Fix build errors

--- src/metadata.js
+++ src/metadata.js
@@ -3,10 +3,14 @@ import pkg from '../package.json';
 
 const layoutNames = {};
 
+let win;
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line no-undef
+  win = window;
+}
+
 export {
   pkg,
   layoutNames, // gets filled out in routes.js
-  // @ts-ignore
-  // eslint-disable-next-line no-undef
-  window
+  win as window
 };
