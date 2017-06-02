ef68548a24f839625ced4b246a62819f0662a899
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-03 11:33:39 +1200

    Add bottom navigation for posts

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'phenomic';
+import enhanceCollection from 'phenomic/lib/enhance-collection';
 
 import Page from '../Page';
+import HeaderPreview from '../../components/HeaderPreview';
 
 import styles from './index.css';
 
-const Post = (props) => {
+const Post = (props, {collection}) => {
   let {head} = props;
 
   const pageDate = head.date ? new Date(head.date) : null;
@@ -35,6 +38,83 @@ const Post = (props) => {
     <div className={styles.time}>{dateNodes}</div>,
     props.header || undefined
   ]}</div>;
+
+  let footer;
+  if (pageDate) {
+    let prevs = enhanceCollection(collection, {
+      filters: [
+        i => i.layout === head.layout,
+        i => i.date && new Date(i.date) < pageDate,
+        i => !i.hidden
+      ],
+      sort: 'date',
+      reverse: true,
+      limit: 1
+    });
+    let nexts = enhanceCollection(collection, {
+      filters: [
+        i => i.layout === head.layout,
+        i => i.date && new Date(i.date) > pageDate,
+        i => !i.hidden
+      ],
+      sort: 'date',
+      reverse: false,
+      limit: 1
+    });
+    let categoriser = enhanceCollection(collection, {
+      filters: [
+        i => i.layout === 'ListPage',
+        i => i.listFilter && i.listFilter.layout && i.listFilter.layout === head.layout,
+        i => !i.hidden
+      ],
+      sort: 'date',
+      reverse: false,
+      limit: 1
+    });
+
+    let next;
+    let prev;
+    let cat;
+    if (prevs.length) {
+      let i = prevs[0];
+      prev = (
+        <Link to={i.__url} className={styles.navItem} key="prv">
+          <HeaderPreview { ...i } showType type={`Previous ${i.layout}`} />
+        </Link>
+      );
+    } else {
+      prev = <div className={styles.navItem} key="prv" />;
+    }
+    if (categoriser.length) {
+      let i = categoriser[0];
+      cat = (
+        <Link to={i.__url} className={styles.navItem} key="cat">
+          <HeaderPreview { ...i } showType type="Show All" />
+        </Link>
+      );
+    } else {
+      cat = <div className={styles.navItem} key="cat" />;
+    }
+    if (nexts.length) {
+      let i = nexts[0];
+      next = (
+        <Link to={i.__url} className={styles.navItem} key="nxt">
+          <HeaderPreview { ...i } showType type={`Next ${i.layout}`} />
+        </Link>
+      );
+    } else {
+      next = <div className={styles.navItem} key="nxt" />;
+    }
+
+    footer = (
+      <div className={styles.bottomNav}>
+        {prev}
+        {cat}
+        {next}
+      </div>
+    );
+    
+  }
 
   return (
     <Page
