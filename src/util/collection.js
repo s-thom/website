2823ab83f602f8056f683b34f4a984475ea2e525
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-03 13:12:50 +1200

    Begin collection utility

---
+++ src/util/collection.js
@@ -0,0 +1,15 @@
+export const filters = {
+  
+};
+
+export const sort = {
+  
+};
+
+export function filter(collection, filters, sorter = 'title', limit = 0) {
+
+}
+
+export function parent(collection, url) {
+
+}
