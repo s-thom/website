8a97b33f3787326a33b486075c0b811f55c985f1
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-26 15:51:04 +1200

    Create header previews

--- src/util/reactify.js
+++ src/util/reactify.js
@@ -1,3 +1,36 @@
-export default function reactify(text) {
-  
+import React from 'react';
+
+import {Link} from 'phenomic';
+import HeaderList from '../components/HeaderList';
+
+import {self}  from './collection';
+
+const headerListRegex = /<!--\s?HEADER-PREVIEW\s([\w/\\.-]+)\s?-->/;
+
+/**
+ * Finds template comments and turns them into proper elements
+ * 
+ * @export
+ * @param {string} body Body content
+ */
+export default function reactify(body, collection) {
+  if (!body) {
+    return undefined;
+  }
+  return body
+    .split(/\r?\n/g)
+    .map((text) => {
+      let match;
+      match = text.match(headerListRegex);
+      if (match) {
+        let url = match[1];
+        let page = self(collection, url);
+        if (page) {
+          return <Link to={page.__url} ><HeaderList pages={[page]} showTypes={false} /></Link>;
+        }
+      }
+
+      // If nothing matched, return the text
+      return text;
+    });
 }
