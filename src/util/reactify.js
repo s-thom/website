f6cf6e5a7f2dc93cf48da139ae58e7c4c3539517
Stuart Thomson <stu.thom.96@gmail.com>
2017-06-26 15:58:27 +1200

    Compress array
    Reduces the number of elements, plus prevents problems when having multiple lines is important (e.g. lists or custom HTML (such as the <figure> image wrapper)).

--- src/util/reactify.js
+++ src/util/reactify.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import {Link} from 'phenomic';
 import HeaderList from '../components/HeaderList';
 
 import {self}  from './collection';
@@ -26,11 +25,30 @@ export default function reactify(body, collection) {
         let url = match[1];
         let page = self(collection, url);
         if (page) {
-          return <Link to={page.__url} ><HeaderList pages={[page]} showTypes={false} /></Link>;
+          return <HeaderList pages={[page]} showTypes={false} />;
         }
       }
 
       // If nothing matched, return the text
       return text;
-    });
+    })
+    .reduce((arr, curr) => {
+      if (typeof curr === 'string') {
+        let last = arr[arr.length - 1];
+        if (!last) {
+          arr.push(curr);
+          return arr;
+        } else if (typeof last === 'string') {
+          let newLast = `${last}\n${curr}`;
+          arr[arr.length - 1] = newLast;
+          return arr;
+        } else {
+          arr.push(curr);
+          return arr;
+        }
+      } else {
+        arr.push(curr);
+        return arr;
+      }
+    }, []);
 }
