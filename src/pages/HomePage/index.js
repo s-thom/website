da76a01263b2ef011e0bda2c0b7629e351fddc35
Stuart Thomson <stu.thom.96@gmail.com>
2017-09-03 12:12:30 +1200

    Add Header to homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -9,6 +9,7 @@ import Link from '@phenomic/plugin-renderer-react/lib/components/Link';
 
 import Page from '../Page';
 
+import Header from '../../components/Header';
 import HeaderList from '../../components/HeaderList';
 
 export default function HomePage({
@@ -20,7 +21,7 @@ export default function HomePage({
 }) {
   let page = {
     node: {
-      title: 'Stuart Thomson',
+      title: 'Hi, I\'m Stuart Thomson',
       img: '/assets/img/banner.jpg',
     },
     status: 'idle',
@@ -28,13 +29,14 @@ export default function HomePage({
 
   return (
     <Page {...page} url={pathname}>
+      <Header head={page.node} url={pathname} />
       <p>I'm a random person on the internet! I've been to the <Link to="https://waikato.ac.nz">University of Waikato</Link> to do a Bachelor's of Computing and Mathematical Sciences. I enjoy writing code, playing music, and walking (over mountains, through forests, around gardens. Anything really).</p>
       <p>You'll find a bunch of different projects over at <Link to="https://github.com/s-thom">GitHub</Link>. If you've played games with me online, you'll know there's another GitHub account I use for that with even more.</p>
       <p>This website is still rather quiet, and I'd like to do more with it. I'm in the process of converting/creating it, so it will get some love.</p>
       <h1>Recent Posts</h1>
       {!isLoading && <HeaderList root="/posts/" pages={posts.node.list} />}
       <h1>Recent Projects</h1>
-      {!isLoading && <HeaderList root="/posts/" pages={projects.node.list} />}
+      {!isLoading && <HeaderList root="/projects/" pages={projects.node.list} />}
     </Page>
   );
 }
