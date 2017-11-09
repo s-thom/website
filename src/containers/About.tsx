880cc4310a78a1e17224659b714a41b59b795e41
Stuart Thomson <stu.thom.96@gmail.com>
2017-11-10 08:11:08 +1300

    Remove silly template code

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-// Example of using paths alias via tsconfig.json to easily re-use legacy code
-// Also, use TypeScript and JavaScript interchangeably.
-import aliasExample from '@myPathAlias/component';
-//
-
 export default () => (
   <div>
     <h1>This is what we're all about.</h1>
