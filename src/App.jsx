ed93c87d7fcd15e0bde9efa7e321e4fa90b39712
Stuart Thomson <stu.thom.96@gmail.com>
2017-11-09 20:56:32 +1300

    Add PageHeader

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import { Router, Link } from 'react-static';
 
 import Routes from 'react-static-routes';
 
+import PageHeader from './components/PageHeader';
 import PageFooter from './components/PageFooter';
 
 import './app.css';
+import './include/index.global.css';
+import './include/highlight.global.css';
 
 export default () => (
   <Router>
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
+      <PageHeader />
       <div className="content">
         <Routes />
       </div>
