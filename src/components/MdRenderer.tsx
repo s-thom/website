5451592b6ca5730623605c81668321b9c37332f7
Stuart Thomson <stu.thom.96@gmail.com>
2017-11-09 16:46:28 +1300

    Clean up markdown stuff

--- src/components/MdRenderer.tsx
+++ src/components/MdRenderer.tsx
@@ -16,8 +16,13 @@ import HrefLink from './HrefLink';
 
 type ComponentType = string | React.ComponentType<any>;
 
-interface OptionsType {
-  components?: { [key: string]: ComponentType };
+interface ComponentMap { 
+  [x: string]: ComponentType;
+}
+
+interface Props {
+  text: string;
+  components?: ComponentMap;
 }
 
 type ItemType =
@@ -31,50 +36,8 @@ type ItemType =
     c: ItemType | ItemType[],
   };
 
-const defaultOptions: OptionsType = {
-  components: {
-    a: HrefLink,
-  },
-};
-
-function renderItems(item: ItemType, options: OptionsType, key?: any): string | JSX.Element | null {
-  if (!item) {
-    return null;
-  }
-  if (typeof item === 'string') {
-    console.log('is string');
-    return item;
-  }
-  const { p: props = {}, c: children } = item;
-  // tslint:disable-next-line variable-name
-  const Tag =
-    (options.components && item.t && options.components[item.t]) ||
-    item.t ||
-    'div';
-
-  return (
-    <Tag {...props} key={key}>
-      {Array.isArray(children)
-        ? children.map((child: ItemType, key) => renderItems(child, options, key))
-        : renderItems(children, options)}
-    </Tag>
-  );
-}
-
-interface Props {
-  text: string;
-  components?: {
-    [x: string]: ComponentType;
-  };
-}
-
-// here we optimize structure just a little to have to smallest json possible
-const createElement = (component: string, props: any, children: any) => {
-  return {
-    t: component,
-    ...(props && Object.keys(props).length ? { p: props } : {}),
-    ...(children ? { c: children } : {}),
-  };
+const defaultComponents: ComponentMap = {
+  a: HrefLink,
 };
 
 const processor = unified()
@@ -83,21 +46,56 @@ const processor = unified()
   .use(rehypeRaw)
   .use(rehypeReact, { createElement });
 
+function renderItems(
+  item: ItemType, 
+  components: ComponentMap,
+  key?: any,
+): string | JSX.Element | null {
+  if (!item) {
+    return null;
+  }
+  if (typeof item === 'string') {
+    return item;
+  }
+  const { p: props = {}, c: children } = item;
+  // tslint:disable-next-line variable-name
+  const Tag =
+    (components && item.t && components[item.t]) ||
+    item.t ||
+    'div';
+
+  return (
+    <Tag {...props} key={key}>
+      {Array.isArray(children)
+        ? children.map((child: ItemType, key) => renderItems(child, components, key))
+        : renderItems(children, components)}
+    </Tag>
+  );
+}
+
+
+// here we optimize structure just a little to have to smallest json possible
+// Taken from Phenomic
+function createElement(component: string, props: any, children: any) {
+  return {
+    t: component,
+    ...(props && Object.keys(props).length ? { p: props } : {}),
+    ...(children ? { c: children } : {}),
+  };
+}
+
 // tslint:disable-next-line function-name
 export default function MdRenderer({
   text,
   components,
 }: Props) {
-  const processed = processor.processSync(text).contents;
-  console.log(processed);
-
-  const r = renderItems(processed, {
-    ...defaultOptions,
-    // force to mix components, as default one (Link) is crucial
-    components: {
-      ...defaultOptions.components,
+  const r = renderItems(
+    processor.processSync(text).contents, 
+    {
+      // force to mix components, as default one (Link) is crucial
+      ...defaultComponents,
       ...(components || {}),
     },
-  });
+  );
   return typeof r === 'string' ? <div>{r}</div> : r;
 }
